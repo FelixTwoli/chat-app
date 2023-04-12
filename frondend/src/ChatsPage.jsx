0031c147cd5203415cd60b9a341ac7a2de133e3d
Felix <felixtwoli21@gmail.com>
2023-04-12 17:02:56 +0300

    Update ChatsPage component to use react-chat-engine-advanced

--- frondend/src/ChatsPage.jsx
+++ frondend/src/ChatsPage.jsx
@@ -1,5 +1,12 @@
-const ChatsPage = () => {
-    return <>chats....</>
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced'
+
+
+const ChatsPage = (props) => {
+    const ChatProps = useMultiChatLogic('8e49a312-5b6d-4121-9852-9f666ec6b7ec', props.user.username, props.user.secret)
+    return (<div style={{ height: '100vh' }}>
+        <MultiChatSocket {...ChatProps} />
+        <MultiChatWindow {...ChatProps} style={{ height: '100%' }} />
+    </div>)
 }
 
 export default ChatsPage
